3f9d9c21c72c1f8e54e74b74ba8708813201b1b4
siebenbocklandt <sieben.bocklandt@student.kuleuven.be>
2019-05-09 00:17:03 +0200

    added boolean

--- agent.py
+++ agent.py
@@ -41,7 +41,7 @@ class Agent:
     def next_action(self, player):
       if not self.loaded:
         self.agent.load('decision_logic/model_output/dqn_agent/Weights')
-      nm = self.agent.next_action(player,self.players,self.apples)
+      nm = self.agent.next_action(player,self.players,self.apples,False)
         # if agenttype == 'greedy':
         #     nm = greedy.get_greedy_decision(player, self.players, self.apples)
         # elif agenttype == 'random':
