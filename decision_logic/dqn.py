2ad376ba266d312e65cd726b088adc2c5108c92b
siebenbocklandt <sieben.bocklandt@student.kuleuven.be>
2019-05-08 21:44:21 +0200

    dqn next_action

--- decision_logic/dqn.py
+++ decision_logic/dqn.py
@@ -6,12 +6,15 @@ from keras.layers import Dense
 from keras.layers import Conv2D
 from keras.optimizers import Adam
 from keras.models import Sequential
+from decision_logic import oh_encoding as ohe
 
 
+#array of the actions that can be played
+ACTIONS = ["move","left","right","fire"]
 # Position, orientation, apples, other players (orientation/location)
 state_size = 15*15*6
 
-# move, left, right
+# move, left, right, fire
 action_size = 4
 
 # Hyper-paramater (power of 2)
@@ -41,6 +44,10 @@ class DQNAgent():
         # Epsilon floor as to not stop exploring
         self.epsilon_min = 0.01
         self.learning_rate = 0.001
+        self.state = None
+        self.previous_state = None
+        self.previous_reward=None
+        self.previous_action=None
 
         self.model = self._build_model()
 
@@ -49,7 +56,7 @@ class DQNAgent():
         model.add(Conv2D(64, (2,2), input_shape=(6, 15, 15)))
         model.add(Dense(24, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        model.compile(loss='cross-entropy', optimizer=Adam(lr=self.learning_rate))
         return model
 
     def remember(self, state, action, reward, next_state, done):
@@ -80,3 +87,75 @@ class DQNAgent():
 
     def save(self, name):
         self.model.save_weights(name)
+
+    def next_action(self,player,players,apples):
+      self.state = ohe.encode_state(player,players,apples) #state to one hot encoding
+      #now we know the next state, we can train the model
+      self.remember(self.previous_state, self.previous_action, self.previous_reward, self.state, False) #train based on the previous action
+      if len(self.memory) > batch_size:
+        self.replay(batch_size)  # train the agent by replaying the experiences of the episode
+      action = self.act(self.state)
+      self.previous_reward = self.get_reward_after_action(player,players,apples,ACTIONS[action])
+      self.previous_action=action
+      self.previous_state = self.state
+      return ACTIONS[action]
+
+
+
+    def get_reward_after_action(self,player,players,apples,action):
+      orientation = players[player - 1].get('orientation')
+      x = players[player - 1].get('location')[0]
+      y = players[player - 1].get('location')[1]
+      reward = 0
+      if action == "move":
+        if orientation == "up":
+          newx = x
+          newy = (y - 1) % 16
+        elif orientation == "left":
+          newx = (x - 1) % 36
+          newy = y
+        elif orientation == "right":
+          newx = (x + 1) % 36
+          newy = y
+        else:
+          newx = x
+          newy = (y + 1) % 16
+
+      elif action == "left":
+        if orientation == "up":
+          newx = (x - 1) % 36
+          newy = y
+        elif orientation == "left":
+          newx = x
+          newy = (y + 1) % 16
+        elif orientation == "right":
+          newx = x
+          newy = (y - 1) % 16
+        else:
+          newx = (x + 1) % 36
+          newy = y
+      elif action == "right":
+        if orientation == "up":
+          newx = (x + 1) % 36
+          newy = y
+        elif orientation == "left":
+          newx = x
+          newy = (y - 1) % 16
+        elif orientation == "right":
+          newx = x
+          newy = (y + 1) % 16
+        else:
+          newx = (x - 1) % 36
+          newy = y
+      else:
+        newx = x
+        newy = y
+        reward -= 1
+      if newx == 0:
+        newx = 36
+      if newy == 0:
+        newy = 16
+      if [newx, newy] in apples:
+        reward += 1
+      return reward
+
