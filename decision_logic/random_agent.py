8bce662a22bf3ac637d720d7e23137f20d2e4518
WToon <toonwillemot@live.nl>
2019-05-08 20:14:20 +0200

    game_env finished.

--- decision_logic/random_agent.py
+++ decision_logic/random_agent.py
@@ -1,10 +1,8 @@
 import random
-import numpy as np
+
+
 class RandomAgent:
-  def __init__(self):
-    self.actions=np.zeros(4)
 
   def next_action(self,players, apples):
     nm = random.choices(['move','left','right','fire'])
-    self.actions[['move','left','right','fire'].index(nm[0])]+=1
     return nm[0]
